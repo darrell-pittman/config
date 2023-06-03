5fc6c481c3fa1a5c82d341bea47934929aebb843
Darrell Pittman <darrellpittman@hotmail.com>
2023-06-03 18:23:11 -0230

    fixed gnucash custom reports

--- gnucash/config-user.scm
+++ gnucash/config-user.scm
@@ -1,7 +1,4 @@
 ; Link or copy this file to ~/.config/gnucash/
 ; and Link or copy reports to ~/.local/share/gnucash/
-
-(load (gnc-build-userdata-path "report-utils.scm"))
-(load (gnc-build-userdata-path "account-over-time.scm"))
-(load (gnc-build-userdata-path "account-change.scm"))
+(load (gnc-build-userdata-path "expense-chart-total.scm"))
 
