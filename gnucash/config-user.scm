dba7ca763e4fa8fbeba984b4bb473a1e8cbb9436
Darrell Pittman <darrellpittman@hotmail.com>
2023-09-26 19:08:14 -0230

    gnucash cfg for flatpak

--- gnucash/config-user.scm
+++ gnucash/config-user.scm
@@ -1,4 +1,8 @@
 ; Link or copy this file to ~/.config/gnucash/
 ; and Link or copy reports to ~/.local/share/gnucash/
+;
+; For flatpak: 
+;    link or copy this file to ~/.var/app/org.gnucash.GnuCash/config/gnucash/
+;    and link or copy reports to ~/.var/app/org.gnucash.GnuCash/data/gnucash/
 (load (gnc-build-userdata-path "expense-chart-total.scm"))
 
